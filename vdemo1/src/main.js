90b779936348578c072199cb5bb61603559038f4
xxl-007 <1186479152@qq.com>
2024-05-08 11:29:56 +0800

    初始化项目

--- vdemo1/src/main.js
+++ vdemo1/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
 import router from './router'
 import store from './store'
+const app = createApp(App);
 
-createApp(App).use(store).use(router).mount('#app')
+app.use(store).use(router).use(Antd).mount('#app')
