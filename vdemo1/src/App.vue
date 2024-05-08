63aaf7980509374c0a36dcbaddc749302760439f
xxl-007 <1186479152@qq.com>
2024-05-08 11:26:58 +0800

    项目初始化,最简单的结构初始化

--- vdemo1/src/App.vue
+++ vdemo1/src/App.vue
@@ -1,13 +1,9 @@
-<!--
- * @Author: xxl-007 1186479152@qq.com
- * @Date: 2024-05-08 09:46:41
- * @LastEditors: xxl-007 1186479152@qq.com
- * @LastEditTime: 2024-05-08 09:54:53
- * @FilePath: /mytestdemos/mydemos/vdemo1/src/App.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
-    <router-view/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
 </template>
 
 <style>
--- vdemo1/src/views/HomeView.vue
+++ vdemo1/src/views/HomeView.vue
@@ -1,14 +1,7 @@
-<!--
- * @Author: xxl-007 1186479152@qq.com
- * @Date: 2024-05-08 09:46:41
- * @LastEditors: xxl-007 1186479152@qq.com
- * @LastEditTime: 2024-05-08 10:12:52
- * @FilePath: /mytestdemos/mydemos/vdemo1/src/views/HomeView.vue
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
--->
 <template>
   <div class="home">
-   home
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
